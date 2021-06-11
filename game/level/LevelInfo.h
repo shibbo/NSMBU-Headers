5f3fb5b05d58a077a19fa2502610d8eaf48705c9
shibbo <jtandrew@umes.edu>
2021-06-11 01:31:14 -0400

    fix LevelInfo tabbing

--- game/level/LevelInfo.h
+++ game/level/LevelInfo.h
@@ -22,18 +22,18 @@ public:
     static void createInstance(sead::Heap *);
     static LevelInfo* sInstance;
 
-    u8 mWorldNumber; // _10
-    u8 mLevelNumber; // _11
-    u8 mCurrentAreaID; // _12
-    u8 mCurrentEntranceID; // _13
-    u8 mCurrentZoneID; // _14
+    u8 mWorldNumber;                // _10
+    u8 mLevelNumber;                // _11
+    u8 mCurrentAreaID;              // _12
+    u8 mCurrentEntranceID;          // _13
+    u8 mCurrentZoneID;              // _14
     u8 _15;
     u8 _16;
     u8 _17;
-    u32 mEntranceKind; // _18
-    u8 mDestinationWorld; // _1C
-    u8 mDestinationLevel; // _1D
-    u8 mDestinationArea; // _1E
-    u8 mDestinationEntrance; // _1F
-    u32 mDestinationEntranceType; // _20
+    u32 mEntranceKind;              // _18
+    u8 mDestinationWorld;           // _1C
+    u8 mDestinationLevel;           // _1D
+    u8 mDestinationArea;            // _1E
+    u8 mDestinationEntrance;        // _1F
+    u32 mDestinationEntranceType;   // _20
 };
